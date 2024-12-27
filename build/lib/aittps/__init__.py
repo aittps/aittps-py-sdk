ad50d3c60a96151402950b72ddcb443e3ef1b8bf
aittps <aittpsaittps@gmail.com>
2024-12-28 01:24:05 +0530

    Delete build/lib directory

--- build/lib/aittps/__init__.py
+++
@@ -1,1 +0,0 @@
-from .aittps import AITTPS
--- build/lib/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file can be empty or used to set up any test-specific functionality
-# For example, if you have common test helpers, you can import them here.
-
-# For simple testing, no content is needed, but you could import shared utilities if required
